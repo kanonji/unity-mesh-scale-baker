90667e5cf6159fa0d310ea86631694e0df41f27e
kanonji <kanonji@users.noreply.github.com>
2017-05-01 04:24:46 +0900

    First implementation

---
+++ Assets/MeshScaleBaker/Editor/MeshScaleBakerWindow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Kanonji.MeshScaleBaker {
+	public class MeshScaleBakerWindow : EditorWindow {
+		public GameObject source;
+
+		[MenuItem("Tools/MeshScaleBaker")]
+		static void Open() {
+			GetWindow<MeshScaleBakerWindow>();
+		}
+
+		void OnGUI() {
+			this.source = (GameObject)EditorGUILayout.ObjectField("GameObject having mesh.", this.source, typeof(GameObject), true);
+			if (GUILayout.Button("Bake")) {
+				if (this.source == null) {
+					ShowNotification(new GUIContent("No object selected"));
+					return;
+				}
+				var meshScaleBaker = new MeshScaleBaker(this.source);
+				meshScaleBaker.Bake();
+			}
+		}
+	}
+}
